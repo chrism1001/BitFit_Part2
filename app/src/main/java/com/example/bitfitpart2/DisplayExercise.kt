253bd016e37f1b227239797e7c0cda9960a3820a
Christopher Mejia <58187990+chrism1001@users.noreply.github.com>
2023-10-20 15:50:21 -0400

    Create Display file for exercises.

---
+++ app/src/main/java/com/example/bitfitpart2/DisplayExercise.kt
@@ -0,0 +1,8 @@
+package com.example.bitfitpart2
+
+data class DisplayExercise(
+    val name: String?,
+    val date: String?,
+    val sets: Int,
+    val repetitions: Int
+) : java.io.Serializable
